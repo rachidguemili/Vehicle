23e78a30f0f1cbbbc801cd21c25513d1f38714cb
rachidguemili <63862170+rachidguemili@users.noreply.github.com>
2020-05-03 03:43:31 +0200

    Update Car.java
       // ici on override une methode de la superclasse
        //   @Override
            @Override

--- Car.java
+++ Car.java
@@ -4,6 +4,11 @@ public class Car extends Vehicle{
     public Car(String name , int km){
         super(name, km);
     }
+    
+   // ici on override une methode de la superclasse
+    //   @Override
+        @Override
+
     public  String doStuff(){
 
     return "I am " +  this.brand + " and I do  Vroom Vroom ! ";
